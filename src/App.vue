6cd755620009a182d87922c379b56e4ad1a4ed63
NaomiRennie <naomi.lily.405@gmail.com>
2019-04-02 14:38:09 +0900

    change colors

--- src/App.vue
+++ src/App.vue
@@ -18,68 +18,74 @@
 <script>
 import Piece from './components/Piece'
 export default {
-    name: 'App',
-    components: {
+   name: 'App',
+   components: {
     Piece
-  },
+   },
    props: {
-    num: {
+     num: {
       type: Number,
       default: 0
-    }
+     }
    },
-  data (){
-    return {
-      board:[[3,3,3,3,3,3,3,3],
-             [3,0,0,0,0,0,0,3],
-             [3,0,0,0,0,0,0,3],
-             [3,0,0,1,2,0,0,3],
-             [3,0,0,2,1,0,0,3],
-             [3,0,0,0,0,0,0,3],
-             [3,0,0,0,0,0,0,3],
-             [3,3,3,3,3,3,3,3]],
-      directions:[[0,-1],[1,0],[0,1],[-1,0],[-1,-1],[1,1],[-1,1],[1,-1]]
-    }
-  },
-  // watch: {
-  //   board: 
-  // },
-  // mounted() {
-  //   this.piece = JSON.parse(localStorage.getItem('pieces')) || [];
-  // },
-  // watch: {
-  //   piece: function(){
-  //     localStorage.setItem('pieces', JSON.stringify(this.piece))
-  //   }
-  // },
+   data () {
+     return {
+       board:[[0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0],
+             [0,0,0,1,2,0,0,0],
+             [0,0,2,2,1,0,0,0],
+             [0,0,2,2,2,0,0,0],
+             [0,2,0,2,0,0,0,0],
+             [0,0,0,0,0,0,0,0]],
+       directions:[[0,-1],[1,0],[0,1],[-1,0],[-1,-1],[1,1],[-1,1],[1,-1]],
+       turn: false
+     }
+   },
    methods: {
-    clickPiece(x, y){
-      // eslint-disable-next-line
-      console.log(this.board, x, 'HHH')
-      let changeColor=[];
-      this.board = [...this.board]
-      this.board[y - 1] = [...this.board[y - 1]]
-      this.directions.forEach((i) => {
-        let xx = x - 1 + i[0]
-        let yy = y - 1 + i[1]
-        let a = y - 1
-        let b = x - 1
-         // eslint-disable-next-line
-      console.log(yy, xx, 'HHH')
-        if(this.board[xx][yy]===1){
-          return changeColor.push({x:b, y:a})
-        }
-        return this.board[a][b] = 0
-      })
-      // eslint-disable-next-line
-      console.log(this.board)
-     changeColor.forEach((i) =>{
-       return this.board[i.y][i.x]=1
-     })
-      
-    }
-  }
-}
+     clickPiece(x, y) {
+       let myColor = '';
+       let oponentColor = '';
+       this.board = [...this.board]
+       this.board[y - 1] = [...this.board[y - 1]]
+       if(this.turn === false) {
+         myColor = 1
+         oponentColor = 2
+         let confirmedPieces = []
+         this.directions.forEach((i) => {
+           let a = y - 1
+           let b = x - 1
+           let c = i[0]
+           let d = i[1]
+           let xx = b + c
+           let yy = a + d
+           let candidates = [];
+           let n = 1; 
+           if(this.board[yy][xx] === oponentColor) {
+             candidates.push({ x:xx, y:yy },{ x:b, y:a })
+             while(candidates.length > 0){
+               if(this.board[yy + d * n][xx + c * n] === 0) {
+                break;
+               }else if (this.board[yy + d * n][xx + c * n] === oponentColor) {
+                candidates.push({ x:xx + c * n, y:yy + d * n });
+                // eslint-disable-next-line 
+                 console.log('hothohtohtohto')
+                n += 1;
+               } else {
+                confirmedPieces.push(...candidates)
+                break;
+               }
+             }
+            confirmedPieces.forEach((i) =>{
+            return this.board[i.y][i.x] = myColor
+             })
+             confirmedPieces.length=0
+           }   
+         })
+       } 
+     }
+   }
+ }
 </script>
 
 <style scoped>
