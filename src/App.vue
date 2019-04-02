cf8a1c851ce2c7955f23f58a90b6796897e8258d
NaomiRennie <naomi.lily.405@gmail.com>
2019-04-02 15:11:24 +0900

    made turn

--- src/App.vue
+++ src/App.vue
@@ -34,21 +34,28 @@ export default {
              [0,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0],
              [0,0,0,1,2,0,0,0],
-             [0,0,2,2,1,0,0,0],
-             [0,0,2,2,2,0,0,0],
-             [0,2,0,2,0,0,0,0],
+             [0,0,0,2,1,0,0,0],
+             [0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0]],
        directions:[[0,-1],[1,0],[0,1],[-1,0],[-1,-1],[1,1],[-1,1],[1,-1]],
-       turn: false
+       turn: 1
+     }
+   },
+   watch: {
+     board: function (){
+       this.turn = this.turn *= -1
      }
    },
    methods: {
      clickPiece(x, y) {
+       // eslint-disable-next-line 
+       console.log(this.turn, 'turn')
        let myColor = '';
        let oponentColor = '';
        this.board = [...this.board]
        this.board[y - 1] = [...this.board[y - 1]]
-       if(this.turn === false) {
+       if(this.turn === 1) {
          myColor = 1
          oponentColor = 2
          let confirmedPieces = []
@@ -57,23 +64,23 @@ export default {
            let b = x - 1
            let c = i[0]
            let d = i[1]
-           let xx = b + c
-           let yy = a + d
+           let xx = b + c < 0 || b + c > 7 ? b : b + c
+           let yy = a + d < 0 || a + d > 7 ? a : a + d
            let candidates = [];
            let n = 1; 
            if(this.board[yy][xx] === oponentColor) {
              candidates.push({ x:xx, y:yy },{ x:b, y:a })
              while(candidates.length > 0){
                if(this.board[yy + d * n][xx + c * n] === 0) {
-                break;
+                 break;
                }else if (this.board[yy + d * n][xx + c * n] === oponentColor) {
-                candidates.push({ x:xx + c * n, y:yy + d * n });
-                // eslint-disable-next-line 
+                 candidates.push({ x:xx + c * n, y:yy + d * n });
+                 // eslint-disable-next-line 
                  console.log('hothohtohtohto')
-                n += 1;
+                 n += 1;
                } else {
-                confirmedPieces.push(...candidates)
-                break;
+                 confirmedPieces.push(...candidates)
+                 break;
                }
              }
             confirmedPieces.forEach((i) =>{
@@ -82,7 +89,41 @@ export default {
              confirmedPieces.length=0
            }   
          })
-       } 
+       } else if (this.turn === -1) {
+        myColor = 2
+         oponentColor = 1
+         let confirmedPieces = []
+         this.directions.forEach((i) => {
+           let a = y - 1
+           let b = x - 1
+           let c = i[0]
+           let d = i[1]
+           let xx = b + c < 0 || b + c > 7 ? b : b + c
+           let yy = a + d < 0 || a + d > 7 ? a : a + d
+           let candidates = [];
+           let n = 1; 
+           if(this.board[yy][xx] === oponentColor) {
+             candidates.push({ x:xx, y:yy },{ x:b, y:a })
+             while(candidates.length > 0){
+               if(this.board[yy + d * n][xx + c * n] === 0) {
+                 break;
+               }else if (this.board[yy + d * n][xx + c * n] === oponentColor) {
+                 candidates.push({ x:xx + c * n, y:yy + d * n });
+                 // eslint-disable-next-line 
+                 console.log('hothohtohtohto')
+                 n += 1;
+               } else {
+                 confirmedPieces.push(...candidates)
+                 break;
+               }
+             }
+            confirmedPieces.forEach((i) =>{
+            return this.board[i.y][i.x] = myColor
+             })
+             confirmedPieces.length=0
+           }   
+         }) 
+       }
      }
    }
  }
